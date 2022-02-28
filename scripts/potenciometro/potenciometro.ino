59c515ca65c22df65879182a368ad477d863c386
jose <macaverde604@gmail.com>
2022-02-28 18:04:29 -0300

    modifica rotina leitura analogica, media de 10 amostras

--- scripts/potenciometro/potenciometro.ino
+++ scripts/potenciometro/potenciometro.ino
@@ -1,6 +1,15 @@
 // Exemplo 1 - Usando potenciometro para fazer leituras analógicas
 // Apostila Eletrogate - KIT START
 
+#define BIT0 0x01
+#define BIT1 0x02
+#define BIT2 0x04
+#define BIT3 0x08
+#define BIT4 0x10
+#define BIT5 0x20
+#define BIT6 0x40
+#define BIT7 0x80
+
 #define sensorPin A0  // define entrada analógica A0
 #define CHECK_BIT(reg,n) ((reg) & (1<<(n))) // verificar estado do bit n no registrador
 
@@ -11,26 +20,28 @@ int leituraAnalogica(){
   // talvez poderia passar o pino como argumento e fazer config de MUX[3:0] adequada
   unsigned short x;
 
-  ADCSRA |= 0x80; // ativa adc, ADEN = 1
+  ADCSRA |= BIT7; // ativa adc, ADEN = 1
 
-  ADCSRA |= 0x40; // single conversion, ADSC = 1
-  while(CHECK_BIT(ADCSRA, 6)){ // espera até ADSC = 0, q significa q a conversao terminou
-    asm("");
+  int media = 0;
+  for (int i = 0; i < 10; i++){
+    ADCSRA |= BIT6; // single conversion, ADSC = 1
+    while(CHECK_BIT(ADCSRA, 6)){ // espera até ADSC = 0, q significa q a conversao terminou
+      asm("");
+    }
+    x = ADC;
+    media += (int)x; //talvez seja ruim somar dps dividir, mas tenho medo de dividir um 10bit
   }
-  x = ADC;
-
-  // ADCSRA &= ~(0x80); // desliga adc?
-
-  return (int)x;
+  media /= 10;
+  
+  ADCSRA &= ~(BIT7); // desliga adc
+  return media;
 }
 
 void setup() {
   // configuracao adc
-  ADMUX |= 0x40; // REFS[1:0] = 01, high reference como avcc
-  // ADMUX |= 0x20; // ADLAR = 1, configuracao 8bits
+  ADMUX |= BIT6; // REFS[1:0] = 01, high reference como avcc
   // config 10bits default (ADMUX), ADLAR = 0
   // entrada A0 default (ADMUX), MUX[3:0] = 0000
-  // prescaler 2 default (ADCSRA), ADPS[2:0] = 000
 
   Serial.begin(9600); // monitor serial - velocidade 9600 Bps
   delay(100); // atraso de 100 milisegundos
